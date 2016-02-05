57f418798d1ab303e2e60528bce15472907f024f
Nathen Harvey <nharvey@chef.io>
2016-02-05 02:24:36 +0100

    Initial iis_demo cookbook
    Includes proper kitchen configuration

---
+++ cookbooks/iis_demo/README.md
@@ -0,0 +1,4 @@
+# iis_demo
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/iis_demo/metadata.rb
@@ -0,0 +1,7 @@
+name 'iis_demo'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures iis_demo'
+long_description 'Installs/Configures iis_demo'
+version '0.1.0'
