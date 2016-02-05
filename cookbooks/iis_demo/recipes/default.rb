c00547771dcb0d90cb6a8210c7247d99c6142f1d
Nathen Harvey <nharvey@chef.io>
2016-02-05 02:52:02 +0100

    Implement the web server
    * tests pass

--- cookbooks/iis_demo/recipes/default.rb
+++ cookbooks/iis_demo/recipes/default.rb
@@ -3,3 +3,15 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+powershell_script 'Install IIS' do
+  code 'add-windowsfeature Web-Server'
+end
+
+template 'c:\inetpub\wwwroot\Default.htm' do
+  source 'Default.htm.erb'
+end
+
+service 'w3svc' do
+  action [:start, :enable]
+end
+
